d73e2a2f30fb5ac27982e3407847470f111d3ab8
Sergio Castañeda Valenciano <sergiocasta23@gmail.com>
2020-08-17 11:13:07 +0000

    navbar and profile icon added

--- src/js/component/dashBoard.js
+++ src/js/component/dashBoard.js
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
 import { Demo1 } from "./demo1";
 import { DemoChart } from "./demochart";
+import { Nav } from "./navbarmenu";
 import "../../styles/dashboard.scss";
 
 export const DashBoard = () => {
 	return (
 		<div className="dashboard">
-			<div className="divdashboard">
-				<Demo1 className="demoagenda" />
-			</div>
-			<div className="divchart">
-				<DemoChart className="demografica" />
-			</div>
+			<Nav>
+				<div className="divdashboard">
+					<Demo1 className="demoagenda" />
+				</div>
+				<div className="divchart">
+					<DemoChart className="demografica" />
+				</div>
+			</Nav>
 		</div>
 	);
 };
